5fa1d0c8fc6939866955b3f416fa66ba70734183
Fernando Lima <lnfernandobr@gmail.com>
2019-08-01 18:23:24 -0400

    removendo arquivos da branch cg que não faziam parte da matéria

--- AC/mips programs/leituraEEscritaDeInteiro.asm
+++
@@ -1,5 +0,0 @@
-addi $v0,$zero,5	#carrega serviço de leitura de inteiros
-syscall					  #chama serviço de leitura
-add $a0,$zero,$v0 #move resultado da leitura para $a0
-addi $v0,$zero,1	#carrega serviço de impressão de inteiros
-syscall						#chama serviço de impressão (p/$a0)
